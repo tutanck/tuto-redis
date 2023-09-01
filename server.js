4b42405e8c1deacdd5429fd05678e7b2af4bb47f
tutanck <tutanck@gmail.com>
2023-09-01 11:59:35 +0200

    Fix bad subject for sent emails

--- server.js
+++ server.js
@@ -1,30 +1,51 @@
 const express = require("express");
 const axios = require("axios");
+const redis = require("redis");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+let redisClient;
+
+(async () => {
+  redisClient = redis.createClient();
+
+  redisClient.on("error", (error) => console.error(`Error : ${error}`));
+
+  await redisClient.connect();
+})();
 
 async function fetchApiData(species) {
   const apiResponse = await axios.get(
     `https://www.fishwatch.gov/api/species/${species}`
   );
-
   console.log("Request sent to the API");
-
   return apiResponse.data;
 }
 
 async function getSpeciesData(req, res) {
   const species = req.params.species;
   let results;
+  let isCached = false;
 
   try {
-    results = await fetchApiData(species);
-    if (results.length === 0) {
-      throw "API returned an empty array";
+    const cacheResults = await redisClient.get(species);
+    if (cacheResults) {
+      isCached = true;
+      results = JSON.parse(cacheResults);
+    } else {
+      results = await fetchApiData(species);
+      if (results.length === 0) {
+        throw "API returned an empty array";
+      }
+      await redisClient.set(species, JSON.stringify(results), {
+        EX: 180,
+        NX: true,
+      });
     }
+
     res.send({
-      fromCache: false,
+      fromCache: isCached,
       data: results,
     });
   } catch (error) {
