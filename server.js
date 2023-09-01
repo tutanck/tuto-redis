3282ff66932f561c8ab81a9789520f7fa56a3f39
tutanck <tutanck@gmail.com>
2023-09-01 10:43:05 +0200

    Fix bad subject for sent emails

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+async function fetchApiData(species) {
+  const apiResponse = await axios.get(
+    `https://www.fishwatch.gov/api/species/${species}`
+  );
+
+  console.log("Request sent to the API");
+
+  return apiResponse.data;
+}
+
+async function getSpeciesData(req, res) {
+  const species = req.params.species;
+  let results;
+
+  try {
+    results = await fetchApiData(species);
+    if (results.length === 0) {
+      throw "API returned an empty array";
+    }
+    res.send({
+      fromCache: false,
+      data: results,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(404).send("Data unavailable");
+  }
+}
+
+app.get("/fish/:species", getSpeciesData);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
